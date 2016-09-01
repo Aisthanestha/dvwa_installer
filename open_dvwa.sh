0f408b41105d150e0cd5f8240503b28293a5f3bb
aisthanestha <ghendrickson47@gmail.com>
2016-09-01 11:18:41 -0500

    Script to open DVWA
    this script opens dvwa so you don’t have to open firefox enter 127….
    and goes to login.php

---
+++ open_dvwa.sh
@@ -0,0 +1,4 @@
+echo "[*] Starting Firefox to DVWA\nUsername: admin\npassword: password"
+firefox http://127.0.0.1/login.php &> /dev/null &
+echo "Done!"i
+echo "[\033[1;32m*\033[1;37m] DVWA has opened!\n"i                    
